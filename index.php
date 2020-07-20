c4caea6daea991fb7d75733958d3881a3c22d906
xuanzongwu <t3742238@gmail.com>
2020-07-20 17:00:55 +0800

    JS及CSS框架
    Signed-off-by: xuanzongwu <t3742238@gmail.com>

--- index.php
+++ index.php
@@ -4,18 +4,30 @@ if (isset($_SESSION["page"])) {
 } ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>論壇</title>
     <meta charset="utf-8">
+    <?php require('css.php') ?>
 </head>
 
 <body>
 
-<?php require("_header.php"); ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-12">row1</div>
+            <div class="col-lg-6 col-md-6 col-sm-12">
+            <?php require("_header.php"); ?>
 
 <?php require("_nav.php"); ?>
 
 <?php require("latestArticles.php"); ?>
+            </div>
 
+        </div>
+    </div>
+
+    <?php require('js.php') ?>
 </body>
+
 </html>
